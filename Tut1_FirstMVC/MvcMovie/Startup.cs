aa34863bb9777235cb028925a4fe950570bb3290
Puck-The-Bard <zdmcguire1@buffs.wtamu.edu>
2020-02-01 02:40:35 -0600

    added connection strings

--- Tut1_FirstMVC/MvcMovie/Startup.cs
+++ Tut1_FirstMVC/MvcMovie/Startup.cs
@@ -16,20 +16,33 @@ namespace MvcMovie
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {   
+            Environment = env;
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment {get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddControllersWithViews();
 
             services.AddDbContext<MvcMovieContext>(options =>
-                options.UseSqlite(Configuration.GetConnectionString("MovieContext")));
+            {
+                var connectionString = Configuration.GetConnectionString("MvcMoviecontext");
+
+                if (Environment.IsDevelopment())
+                {
+                    options.UseSqlite(connectionString);
+                }
+                else
+                {
+                    options.UseSqlServer(connectionString);
+                }
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
